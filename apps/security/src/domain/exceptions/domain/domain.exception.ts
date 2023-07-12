74647d3cee1761f37ee2a7e32ca0df33f017adf8
Julian Lasso <julian.lasso@sofka.com.co>
2023-07-12 18:34:02 -0500

    [Update] Se actualiza el manejo de la excepción de dominio

--- apps/security/src/domain/exceptions/domain/domain.exception.ts
+++ apps/security/src/domain/exceptions/domain/domain.exception.ts
@@ -1,1 +1,14 @@
-export class DomainException extends Error {}
+import { ValueObjectExceptionType } from '../types';
+
+export class DomainException extends Error {
+  private _errors: ValueObjectExceptionType[];
+
+  constructor(message: string, errors?: ValueObjectExceptionType[]) {
+    super(message);
+    if (errors) this._errors = errors;
+  }
+
+  get errors(): ValueObjectExceptionType[] {
+    return this._errors;
+  }
+}
